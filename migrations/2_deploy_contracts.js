22451122a58c2d629a04611eddad30ff6650d3a7
Vineeth Chelur <crvineeth97@gmail.com>
2019-09-17 18:25:22 +0530

    Fix contract deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var HighLow = artifacts.require("./HighLow.sol");
 
 // Bidding time for each round
-var biddingTime = 50
+var biddingTime = 30
 
 // Reveal time for each round
 var revealTime = 10
@@ -10,5 +10,5 @@ var revealTime = 10
 var houseAddress = "0x5b327Fd9d785a7fF939f35430Db0fcEDd1a350b6"
 
 module.exports = function(deployer) {
-  deployer.deploy(HighLow, biddingTime, revealTime, houseAddress);
+  deployer.deploy(HighLow, biddingTime, revealTime, houseAddress, {from: houseAddress});
 };
